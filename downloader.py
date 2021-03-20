78d1c28c055a0a6ad902dbc5856e0ec420f6e1a2
RWC <david.medina1@udea.edu.co>
2021-03-20 11:07:39 -0500

    downloader and make_paths

---
+++ downloader.py
@@ -0,0 +1,26 @@
+# module for download documents announcements
+import os
+import urllib.request
+from urllib.error import URLError
+
+def download_document(download_url, path_store, file_name):
+    ''' Download a specific pdf alocated in
+        download_url and store in path_store_ with a specific
+        file_name.
+    '''
+
+    try:
+
+        response = urllib.request.urlopen(download_url)
+        file = open(path_store+file_name,'wb')
+        file.write(response.read())
+        file.close()
+    
+    except URLError as e:
+
+        print(e)
+        pass            # flag: that document pdf url of thar announcement don´t work
+                        # build store fail information somewhere...
+
+if __name__=="__main__":
+    pass
---
+++ make_paths.py
@@ -0,0 +1,11 @@
+# this module make paths for store and retrieve documents
+
+import os
+
+def make_store_path(parameter_1,parameter2):
+    # in construction
+    return path_store
+
+def make_rpath_documents(parameter_one, parameter_2):
+    # in construction
+    return path_document
