b2ef874cf8bdde8a74ac353825b56244d82930e4
RWC <david.medina1@udea.edu.co>
2021-03-20 12:27:13 -0500

    function for retrieve files and transform

--- make_paths.py
+++ make_paths.py
@@ -6,6 +6,17 @@ def make_store_path(parameter_1,parameter2):
     # in construction
     return path_store
 
-def make_rpath_documents(parameter_one, parameter_2):
-    # in construction
-    return path_document
+def make_rpath_documents(path_db):  # path to fisic db
+    ''' Make paths to each file for each announcement in each country
+        and transfor these files from pdf to txt.
+    '''
+
+    for root, dirs, files in os.walk(path_db):
+
+        for _root, _dirs, _files in os.walk(root):
+            if files:
+                for _file in _files
+                    # join _root with _file and transform pdf to txt!
+                    # print somet flag...
+        
+        return 'work done for %s country ' % dirs
